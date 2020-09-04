c04a7615645986f1aee61e995c91baedfa64e99c
HoopDead <polec0@wp.pl>
2020-09-04 13:04:37 +0200

    Added bootstrap, fontawesome and custom style + base to extend html

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('login', views.login_page, name = 'login'),
     path('register', views.register_page, name = 'register')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
