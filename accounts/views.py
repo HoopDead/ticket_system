b7391380bdb6358eee35b90c295985bcfdf6e41b
HoopDead <polec0@wp.pl>
2020-09-08 13:16:19 +0200

    Added test function for login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password) # Django Function check if user exist
+
+        if user is not None:
+            login(request, user)
+        else:
+            messages.error(request, 'Nazwa użytkownika lub hasło jest niepoprawne!')
     return render(request, 'login.html')
 
 def register_page(request):
