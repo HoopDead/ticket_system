36703e443cc9b6d06af0ecc525e38190ebec10f0
HoopDead <polec0@wp.pl>
2020-10-12 20:35:25 +0200

    Created login functionality

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse, resolve
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
 class TestPagesViews(TestCase):
@@ -16,3 +18,18 @@ class TestPagesViews(TestCase):
         url = reverse("register")
         response = client.get(url)
         assert response.status_code == 200
+
+
+class LogInTest(TestCase):
+
+    def setUp(self):
+        self.credentials  = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+        User.objects.create_user(**self.credentials )
+
+    def test_login(self):
+        client = Client()
+        logged_in = client.login(**self.credentials)
+        self.assertTrue(logged_in)
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from django.contrib import messages
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def login_page(request):
@@ -9,10 +9,15 @@ def login_page(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
 
+        print(username)
+
         user = authenticate(request, username=username, password=password) # Django Function check if user exist
 
         if user is not None:
             login(request, user)
+            print("Logged in!")
+            #TODO: Add redirect to index page for permitted uesr
+            return render(request, 'index.html')
         else:
             messages.error(request, 'Nazwa użytkownika lub hasło jest niepoprawne!')
     return render(request, 'login.html')
