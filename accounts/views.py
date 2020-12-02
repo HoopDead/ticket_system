aba1bab85608748e86a47424019d70ff512c096f
HoopDead <polec0@wp.pl>
2020-12-02 17:52:21 +0100

    Created a frontend for register, translated login information

--- accounts/views.py
+++ accounts/views.py
@@ -9,8 +9,6 @@ def login_page(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
 
-        print(username)
-
         user = authenticate(request, username=username, password=password) # Django Function check if user exist
 
         if user is not None:
@@ -19,7 +17,7 @@ def login_page(request):
             #TODO: Add redirect to index page for permitted uesr
             return render(request, 'index.html')
         else:
-            messages.error(request, 'Nazwa użytkownika lub hasło jest niepoprawne!')
+            messages.error(request, 'User name or password is incorrect!')
     return render(request, 'login.html')
 
 def register_page(request):
